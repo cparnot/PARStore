88bbd8f728292d501a400902b35d079a7011cb42
Charles Parnot <charles.parnot@gmail.com>
2014-04-06 21:15:33 +0200

    Switching to single implementation test file, consistent with the new XCTest framework.

--- Tests/PARStoreTests.h
+++
@@ -1,9 +0,0 @@
-//  PARStoreTests
-//  Created by Charles Parnot on 3/2/13.
-//  Copyright (c) 2013 Charles Parnot. All rights reserved.
-
-#import "PARTestCase.h"
-
-@interface PARStoreTests : PARTestCase
-
-@end
--- Tests/PARStoreTests.m
+++ Tests/PARStoreTests.m
@@ -2,9 +2,14 @@
 //  Created by Charles Parnot on 3/2/13.
 //  Copyright (c) 2013 Charles Parnot. All rights reserved.
 
-#import "PARStoreTests.h"
+#import "PARTestCase.h"
 #import "PARStoreExample.h"
 #import "PARNotificationSemaphore.h"
+
+@interface PARStoreTests : PARTestCase
+
+@end
+
 
 @implementation PARStoreTests
 
